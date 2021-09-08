0b2ce9e0ae58403a2e87c1db181b21e85626d88a
Salahuddin Shaikh <33448178+salahoddin88@users.noreply.github.com>
2021-09-08 10:59:07 +0530

    answer module

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.homePage, name="homePage"),
     path('category/<int:category_id>', views.categoryQuestion, name='categoryQuestion'),
-    path('question/<int:question_id>', views.QuestionDetails, name='QuestionDetails')
+    path('question/<int:question_id>', views.QuestionDetails, name='QuestionDetails'),
+    path('save-comment', views.SaveComment, name="SaveComment"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
 from category.models import Category
 from question_answer.models import Question, Answer
+from user_profile.models import UserProfile
+from comment.models import Comment
+from django.contrib import messages
+from datetime import datetime
+
 
 
 """ Home page view function """
@@ -26,7 +32,18 @@ def categoryQuestion(request, category_id):
     
 def QuestionDetails(request, question_id):
     navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
-    answers = Answer.objects.filter(question_id=question_id)
+    answersObjects = Answer.objects.filter(question_id=question_id)
+    answers = {}
+    for key, answer in enumerate(answersObjects):
+        try:
+            userProfileObject = UserProfile.objects.get(user=answer.user)
+        except UserProfile.DoesNotExist:
+            userProfileObject = {}
+        
+        answers[key] = {
+            'answer' : answer,
+            'user_profile' : userProfileObject
+        }
     try:
         question = Question.objects.get(id=question_id)
     except Question.DoesNotExist:
@@ -34,5 +51,25 @@ def QuestionDetails(request, question_id):
     return render(request, 'question-details.html', {
         'navigation_categories': navigation_categories,
         'question':question,
-        'answers' : answers
+        'answers' : list(answers.values())
     })
+
+
+def SaveComment(request):
+    questionId = request.POST.get('question_id')
+    answerId = request.POST.get('answer_id')
+    comment = request.POST.get('comment')
+    image = request.FILES.get('image')
+    if comment:
+        Comment.objects.create(
+            answer_id=answerId,
+            comments=comment,
+            added_date=datetime.now(),
+            user=request.user,
+            image = image
+        )
+        messages.success(request, 'Comment added successfully')
+    else:
+        messages.error(request, 'Comment can not be empty')
+    
+    return redirect('QuestionDetails', question_id=questionId)
