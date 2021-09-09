9fe79ab8c4d4a3778dc69a6c4768d3374f8b3672
Salahuddin Shaikh <33448178+salahoddin88@users.noreply.github.com>
2021-09-09 22:16:36 +0530

    Comments, Login, registration, Profile

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('category/<int:category_id>', views.categoryQuestion, name='categoryQuestion'),
     path('question/<int:question_id>', views.QuestionDetails, name='QuestionDetails'),
     path('save-comment', views.SaveComment, name="SaveComment"),
+    path('user-registration', views.userRegistration, name="userRegistration"),
+    path('user-login', views.userLogin, name="userLogin"),
+    path('user-profile', views.userProfile, name="userProfile"),
+    path('post-question', views.postQuestion, name="postQuestion")
 ]
--- account/views.py
+++ account/views.py
@@ -6,8 +6,8 @@ from user_profile.models import UserProfile
 from comment.models import Comment
 from django.contrib import messages
 from datetime import datetime
-
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 """ Home page view function """
 def homePage(request):
@@ -35,6 +35,7 @@ def QuestionDetails(request, question_id):
     answersObjects = Answer.objects.filter(question_id=question_id)
     answers = {}
     for key, answer in enumerate(answersObjects):
+        comments = Comment.objects.filter(answer=answer)
         try:
             userProfileObject = UserProfile.objects.get(user=answer.user)
         except UserProfile.DoesNotExist:
@@ -42,8 +43,10 @@ def QuestionDetails(request, question_id):
         
         answers[key] = {
             'answer' : answer,
-            'user_profile' : userProfileObject
+            'user_profile' : userProfileObject,
+            'comments' : comments
         }
+    
     try:
         question = Question.objects.get(id=question_id)
     except Question.DoesNotExist:
@@ -73,3 +76,113 @@ def SaveComment(request):
         messages.error(request, 'Comment can not be empty')
     
     return redirect('QuestionDetails', question_id=questionId)
+    
+
+def userRegistration(request):
+    if request.method == 'POST':
+        firstName = request.POST.get('first_name')
+        lastName = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('confirm_password')
+        
+        checkUsername = User.objects.filter(username=username)
+        if checkUsername:
+            messages.error(request, 'Username is already taken')
+            return redirect('userRegistration')
+        else:
+            if password == "":
+                messages.error(request, 'Password is required')
+                return redirect('userRegistration')
+                
+            elif confirmPassword == "":
+                messages.error(request, 'Confirm password is required')
+                return redirect('userRegistration')
+            
+            elif password != confirmPassword:
+                messages.error(request, 'Password does not match with confirm password')
+                return redirect('userRegistration')
+            else:
+                user= User.objects.create(
+                    first_name=firstName,
+                    last_name=lastName,
+                    username=username
+                )
+                user.set_password(password)
+                user.save()
+                
+                messages.success(request, 'Congratulation Your registration is successful')
+                return redirect('userLogin')
+        
+    else:
+        navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
+        return render(request, 'user-registration.html', {
+            'navigation_categories':navigation_categories
+        })
+        
+def userLogin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful')
+            return redirect('homePage')
+        else:
+            messages.error(request, 'Invalid Username or password')
+            return redirect('userLogin')
+    else:
+        navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
+        return render(request, 'user-login.html', {
+            'navigation_categories':navigation_categories,
+        })
+
+
+def userProfile(request):
+    if request.method == 'POST':
+        firstName = request.POST.get('first_name')
+        lastName = request.POST.get('last_name')
+        email = request.POST.get('email')
+        mobileNo = request.POST.get('mobile_no')
+        address = request.POST.get('address')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('confirm_password')
+        profilePicture = request.FILES.get('profile_picture')
+        userProfileObject = UserProfile.objects.get(user=request.user)
+        if password != "" and confirmPassword != "":
+            if password == confirmPassword:
+                request.user.set_password(password)
+            else:
+                messages.error(request, 'Password does not match with the confirm password')
+                return redirect('userProfile')
+        user = request.user
+        
+        userProfileObject.address = address
+        userProfileObject.profile_picture = profilePicture
+        messages.success(request, 'Profile is updated')
+        userProfileObject.save()
+        request.user.first_name = firstName
+        request.user.last_name = lastName
+        request.user.email = email
+        userProfileObject.mobile_no = mobileNo
+        request.user.save()
+        login(request, user)
+        return redirect('userProfile')
+    else:
+        navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
+        userProfileObject,_ = UserProfile.objects.get_or_create(user=request.user)
+        return render(request, 'user-profile.html', {
+            'navigation_categories':navigation_categories,
+            'userProfile' : userProfileObject,
+        })
+        
+        
+def postQuestion(request):
+    navigation_categories = Category.objects.filter(status=True).order_by('-id')[:5]
+    categories = Category.objects.filter(status=True)
+    return render(request, 'post-question.html', {
+        'navigation_categories':navigation_categories,
+        'categories' : categories
+        
+    })
